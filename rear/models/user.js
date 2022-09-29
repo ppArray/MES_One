c49772f76daa064e5bb122077c4e7031336dec45
xiaoliutonxue <3107937134@qq.com>
2022-09-29 22:20:47 +0800

    conmit

---
+++ rear/models/user.js
@@ -0,0 +1,63 @@
+ const db=require('../utils/mysqlDB')
+//  电话号码登录验证
+ const phoneLogin= (phone,pas)=>{
+  let sql ='select * from sys_user where phonenumber=? and password=?';
+  db.query(sql,[phone,pas],(err,results)=>{
+    if(err) return err.message;
+    console.log('电话号码：'+results);
+    return results;
+  });
+ }
+//  用户名登录验证
+ const userLogin =(name,pas)=>{
+  let sql ='select * from sys_user where user_name=? and password=?';
+  db.query(sql,[name,pas],(err,data)=>{
+    if(err) return err.message;
+    console.log(data);
+    return data;
+  });
+ }
+//  全部用户查询
+const userQuery =()=>{
+  let sql ='select * from sys_user';
+  db.query(sql,(err,results)=>{
+    if(err) return err.message;
+    return results;
+  });
+}
+// 用户新增方法
+const userAdd =(data)=>{
+  let sql='insert into sys_user (user_name,phonenumber,login_name,sex,dept,email,password,status) values (?,?,?,?,?,?,?,?)';
+  db.query(sql,[data.name,data.phone,data.logName,data.sex,data.dept,data.email,data.pswd,data.status],(err,results)=>{
+    if(err) return err.message;
+    sql='insert into sys_user_role values(?,?)';
+    db.query(sql,[,'2'],(err,results)=>{
+      if(err) return err.message;
+      return results;
+    });
+  });
+}
+// 用户修改方法
+const userUpdate=(data)=>{
+  let sql = 'update sys_user set user_name=?, phonenumber=?,dept=?,email=?,status=?,sex=? where login_name=?';
+  db.query(sql,[data.name,data.phone,data.dept,data.email,data.status,data.sex,data.logName],(err,results)=>{
+    if(err) return err.message;
+    return results;
+  });
+}
+// 用户删除方法
+const userDelete=(data)=>{
+  let sql ='delete from sys_user where user_id=?';
+  db.query(sql,[data.userId],(err,results)=>{
+    if(err) return err.message;
+    return results;
+  });
+}
+ module.exports={
+  phoneLogin,
+  userLogin,
+  userQuery,
+  userAdd,
+  userUpdate,
+  userDelete
+ }
--- rear/routes/users.js
+++ rear/routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
+const {userAdd,userUpdate,userQuery, userDelete}=require('../models/user');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
+// 监听用户增加路由
+router.post('/userAdd',(request,response)=>{
+  const index=userAdd(request.body);
+  // 大于 index 大于 1 增加成功！否则失败！
+  response.send(index);
+});
+// 监听用户修改路由
+router.post('/userUpdate',(request,response)=>{
+  const index=userUpdate(request.body);
+  response.send(index);
+});
+// 监听用户查询路由
+router.post('/userQuery',(request,response)=>{
+  const index=userQuery();
+  response.send(index);
+});
+// 监听用户删除路由
+router.post('/userDelete',(request,response)=>{
+  const index =userDelete(request.body);
+  response.send(index);
+});
 module.exports = router;

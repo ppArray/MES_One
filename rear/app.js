9c98e697ce3fc3158233e1fc3c6060baf78935fe
xiaoliutonxue <3107937134@qq.com>
2022-09-30 10:36:12 +0800

    小刘

--- rear/app.js
+++ rear/app.js
@@ -1,8 +1,5 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,26 +7,20 @@ var authDiddleware = require('./middleware/auth');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login',authDiddleware);
 
-// catch 404 and forward to error handler
+// // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
@@ -40,4 +31,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+app.listen(3000,()=> console.log("http://127.0.0.1:3000"))
--- rear/routes/index.js
+++ rear/routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log('request /');
+  res.json({ status: 200 })
 });
 
 module.exports = router;
